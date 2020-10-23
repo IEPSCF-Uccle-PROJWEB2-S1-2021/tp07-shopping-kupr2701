5b9468612b9b82d540bf5d60796b9a0947906bf0
PROJWEB2 kuifo yimga prisca joel <joelkuifo@gmail.com>
2020-10-23 23:30:58 +0200

    fin

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // Écrivez votre code JavaScript ici.
+let lst = document.querySelector("ul");
+let ipt = document.querySelector("input");
+let btn = document.querySelector("button");
+
+btn.onclick = function () {
+  let valcourt = ipt.value;
+  ipt.value = '';
+
+  let lstli = document.createElement("li");
+  let spnli = document.createElement("span");
+  let btnli = document.createElement("button");
+
+  lstli.appendChild(spnli);
+  spnli.textContent = valcourt;
+  lstli.appendChild(btnli);
+  btnli.textContent = 'delete';
+  lst.appendChild(lstli);
+
+  btnli.onclick = function () {
+    lst.removeChild(lstli);
+  }
+  ipt.focus();
+}
+
